cb1628e0b8d1c60cc7622ce90f9aaf902da83594
theerawit1997 <theerawit.towork@gmail.com>
2025-01-27 08:32:34 +0700

    Create page.jsx

---
+++ src/app/home/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Link from "next/link";
+
+function Homepage() {
+  return (
+    <div>
+      <Navbar />
+      Home page
+    </div>
+  );
+}
+
+export default Homepage;
